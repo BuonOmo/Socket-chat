19bba465778240de67ac8caa88f172d7b7b6764a
KaiserCroc <piebay13@laposte.net>
2015-12-15 15:35:31 +0100

    Create RequeteClient.java

---
+++ src/stream/RequeteClient.java
@@ -0,0 +1,12 @@
+package socket;
+
+public class RequeteClient {
+
+	public String message;
+	public String username;
+	public String otherUsername;
+	
+	public RequeteClient (String message) {
+		this.message = message;
+	}
+}
