6e7920209af97a0f8f0c72217a3a8bd1c1c7e704
KaiserCroc <piebay13@laposte.net>
2015-12-15 16:35:20 +0100

    Update RequeteClient.java

--- src/stream/RequeteClient.java
+++ src/stream/RequeteClient.java
@@ -12,8 +12,43 @@ public class RequeteClient implements Serializable{
 	public String message;
 	public String username;
 	public String otherUsername;
+	public int type;
+	public boolean valide = true;
 	
-	public RequeteClient (String message) {
-		this.message = message;
+	public RequeteClient (String requete) {
+		
+		String req [] = requete.split(" ");
+		switch (req[0])
+		{
+			case ("CONNECT") :
+				if (requete.length() < "CONNECT abcd".length()){
+					System.err.println ("Identifiant inexistant ou trop court");
+					valide = false;
+				}
+				else {
+					type = 0;
+					username = requete.substring("CONNECT ".length());
+				}
+				break;
+				
+			case ("SENDTO") :
+				type=1;
+				if (req.length == 4 && req[2].equals("CONTENT")&&(req[1].equals("all") || req[1].length() > 3))
+				{
+					otherUsername = req[1];
+					message = req[3];
+				}
+				else
+				{
+					System.err.println("Requete d'envoi non valide");
+					valide=false;
+				}
+				break;
+				
+			default :
+				valide=false;
+				break;
+		}
+		
 	}
 }
